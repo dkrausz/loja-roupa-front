d5f9da7b4a6b1e690d81ad3a1553ccb00551be85
Danilo Krausz <danilo.krausz@gmail.com>
2024-12-04 12:39:39 +0100

    Fix vercel issue again

--- src/components/form/addressForm/index.tsx
+++ src/components/form/addressForm/index.tsx
@@ -22,7 +22,7 @@ export function AddAddress() {
   const {
     register,
     handleSubmit,
-    getValues,
+    // getValues,
     formState: {errors, isSubmitting},
   } = useForm<IAddress>({
     defaultValues: selectedAddress
